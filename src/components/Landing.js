f23f329ad9fcc8474514e53c789d9e6a8c04c7a4
harsha126 <harsha.sdm111@gmail.com>
2022-05-08 23:53:51 +0530

    changed the landing page

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -1,11 +1,20 @@
 import React from "react";
-import glogo from "../images/icon-google.png";
+import pic1 from "../images/pic1.jpeg";
+import pic2 from "../images/pic2.jpeg";
+import { Link } from "react-router-dom";
+
 
 export default function Landing() {
   return (
-    <div className="login-container">  
-        This is Landing Page 
-      
+    <div className="land-container">
+      <img src = {pic1}/>
+      <h2> Connect With Your Friends </h2>
+      <div className="land-link">
+        <Link to="/login"> Login </Link>
+      </div>
+      <div className="land-link">
+        <Link to="/signup"> Sign Up </Link>
+      </div>
     </div>
   );
 }
